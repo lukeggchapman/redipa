bac3869b4228927bc00426921507b396c48b77e2
Luke Chapman <lukeggchapman@gmail.com>
2013-08-17 16:21:23 +1000

    Built android platform

--- platforms/android/bin/HelloWorld-debug-unaligned.apk.d
+++ platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -2,3 +2,4 @@
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/HelloWorld.ap_ \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes.dex \
 /Users/Chappo/PhoneGap/redipa/platforms/android/libs/cordova-3.0.0.jar \
+/Users/Chappo/PhoneGap/redipa/platforms/android/libs/com.google.zxing.client.android.captureactivity.jar \
