a1012943d8234856413978be9aac58505a155b96
Luke Chapman <lukeggchapman@gmail.com>
2013-08-17 15:55:24 +1000

    First failed barcode scanner attempt

--- platforms/android/bin/classes.dex.d
+++ platforms/android/bin/classes.dex.d
@@ -1,10 +1,18 @@
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes.dex : \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/BuildConfig.class \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/HelloWorld.class \
+/Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$array.class \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$attr.class \
+/Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$color.class \
+/Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$dimen.class \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$drawable.class \
+/Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$id.class \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$layout.class \
+/Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$menu.class \
+/Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$raw.class \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$string.class \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R$xml.class \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/hello_world/R.class \
+/Users/Chappo/PhoneGap/redipa/platforms/android/bin/classes/com/phonegap/plugins/barcodescanner/BarcodeScanner.class \
 /Users/Chappo/PhoneGap/redipa/platforms/android/bin/dexedLibs/cordova-3.0.0-efdc792bce6225f11091f4bad595cfb9.jar \
+/Users/Chappo/PhoneGap/redipa/platforms/android/bin/dexedLibs/com.google.zxing.client.android.captureactivity-d1dfb3034f85996db611364e3dd5b493.jar \
